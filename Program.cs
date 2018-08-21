15bc4f09b2a357684145df3e69e62d1a2ba0b50e
serge klokov <sergeklokov@users.noreply.github.com>
2018-08-20 17:59:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.DirectoryServices;
+
+namespace LdapConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<UserAdDTO> lstADUsers = new List<UserAdDTO>();
+
+            var domainPath = string.Format("WinNT://{0},computer", Environment.MachineName);
+            Console.WriteLine("Domain path = " + domainPath);
+
+            //string domainPath = "LDAP://DC=xxxx,DC=com";
+
+            DirectoryEntry searchRoot = new DirectoryEntry(domainPath);
+            DirectorySearcher search = new DirectorySearcher(searchRoot);
+            search.Filter = "(&(objectClass=user)(objectCategory=person))";
+            search.PropertiesToLoad.Add("samaccountname");
+            search.PropertiesToLoad.Add("mail");
+            search.PropertiesToLoad.Add("usergroup");
+            search.PropertiesToLoad.Add("displayname");//first name
+            SearchResult result;
+            SearchResultCollection resultCol = search.FindAll();
+
+            if (resultCol != null)
+            {
+                for (int counter = 0; counter < resultCol.Count; counter++)
+                {
+                    string UserNameEmailString = string.Empty;
+                    result = resultCol[counter];
+                    if (result.Properties.Contains("samaccountname") &&
+                             result.Properties.Contains("mail") &&
+                        result.Properties.Contains("displayname"))
+                    {
+                        UserAdDTO objSurveyUsers = new UserAdDTO();
+                        objSurveyUsers.Email = (String)result.Properties["mail"][0] +
+                          "^" + (String)result.Properties["displayname"][0];
+                        objSurveyUsers.UserName = (String)result.Properties["samaccountname"][0];
+                        objSurveyUsers.DisplayName = (String)result.Properties["displayname"][0];
+                        lstADUsers.Add(objSurveyUsers);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ UserAdDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace LdapConsole
+{
+    internal class UserAdDTO
+    {
+        public string Email { get; set; }
+        internal string UserName { get; set; }
+        public string DisplayName { get; set; }
+        
+    }
+}
